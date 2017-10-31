488bfd255fad8601718f3116d479d5308a33c331
ustcxiexk <30454548+ustcxiexk@users.noreply.github.com>
2017-10-31 15:38:20 +0800

    Create tree.py

---
+++ tree.py
@@ -0,0 +1,81 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+        if index >= len(inputValues):
+            break
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+def pre_traversal(root):
+    if root:
+        print root.val
+        pre_traversal(root.left)
+        pre_traversal(root.right)
+def in_traversal(root):
+    if root:
+        in_traversal(root.left)
+        print root.val
+        in_traversal(root.right)
+def post_traversal(root):
+    if root:
+        post_traversal(root.left)
+        post_traversal(root.right)
+        print root.val
+
+####### Applications
+def trimBST(root, L, R):
+    if not root:
+        return None
+    if root.val > R:
+        return trimBST(root.left, L, R)
+    elif root.val < L:
+        return trimBST(root.right, L, R)
+    else:
+        root.left = trimBST(root.left, L, R)
+        root.right = trimBST(root.right, L, R)
+    return root
+
+def mergeTrees(root1, root2):
+    if not root1 and not root2:
+        return None
+    root = TreeNode((root1.val if root1 else 0) + (root2.val if root2 else 0))
+    root.left = mergeTrees(root1 and root1.left, root2 and root2.left)
+    root.right = mergeTrees(root1 and root1.right, root2 and root2.right)
+    return root
+s1 = "[1,3,2,5]"
+s2 = "[2,1,3,null,4,null,7]"
+root1 = stringToTreeNode(s1)
+root2 = stringToTreeNode(s2)
+pre_traversal(root1)
+pre_traversal(root2)
+new_root = mergeTrees(root1, root2)
+pre_traversal(new_root)
+
+
