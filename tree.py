e56001538c5fb313292937d9f3891bd16835772a
ustcxiexk <30454548+ustcxiexk@users.noreply.github.com>
2018-01-24 21:09:15 +0800

    Update tree.py

--- tree.py
+++ tree.py
@@ -89,6 +89,21 @@ def mergeTrees(root1, root2):
     root.right = mergeTrees(root1 and root1.right, root2 and root2.right)
     return root
 
+##########输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+#########例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回.
+def reConstructBinaryTree(pre, tin):
+    if not pre:
+        return None
+    else:
+        t = TreeNode(pre[0])
+        subPreLeft = pre[1 : tin.index(pre[0])+1]
+        subTinLeft = tin[0 : tin.index(pre[0])]
+        t.left = reConstructBinaryTree(subPreLeft, subTinLeft)
+        subPreRight = pre[tin.index(pre[0])+1 :]
+        subTinRight = tin[tin.index(pre[0])+1 :]
+        t.right = reConstructBinaryTree(subPreRight, subTinRight)
+    return t
+
 # ----- new added by zxh -----
 pass
 # ----------------------------
